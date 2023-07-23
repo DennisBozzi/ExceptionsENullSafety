398762c72143ad910533af99a8930bc6c1760fa3
Dennis Bozzi <dennisbozzii@gmail.com>
2023-07-23 09:19:12 -0300

    Entendendo o conceito de pilha de execução

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -1,25 +1,49 @@
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
+// void main() {
+//   // Criando o banco
+//   BankController bankController = BankController();
+//
+//   // Adicionando contas
+//   bankController.addAccount(
+//       id: "Ricarth",
+//       account:
+//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+//
+//   bankController.addAccount(
+//       id: "Kako",
+//       account:
+//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+//
+//   // Fazendo transferência
+//   bool result = bankController.makeTransfer(
+//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+//
+//   // Observando resultado
+//   print(result);
+// }
+
 void main() {
-  // Criando o banco
-  BankController bankController = BankController();
+  print("Started Main");
+  functionOne();
+  print("Finished Main");
+}
 
-  // Adicionando contas
-  bankController.addAccount(
-      id: "Ricarth",
-      account:
-          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+void functionOne() {
+  print("Started F01");
 
-  bankController.addAccount(
-      id: "Kako",
-      account:
-          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+  functionTwo();
 
-  // Fazendo transferência
-  bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+  print("Finished F01");
+}
 
-  // Observando resultado
-  print(result);
+void functionTwo() {
+  print("Started F02");
+
+  for (int i = 0; i <= 5; i++) {
+    print(i);
+  }
+
+  print("Finished F02");
 }
